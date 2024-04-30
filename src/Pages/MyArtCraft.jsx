ad4da18128e331ad8511d00b8dac0e8b358b7b9a
Md Fahim Hossain <fahimreshad3@gmail.com>
2024-04-30 19:36:07 +0600

    completed myartcraft section

--- src/Pages/MyArtCraft.jsx
+++ src/Pages/MyArtCraft.jsx
@@ -1,30 +1,33 @@
-
 import { useEffect, useState } from "react";
 import UseAuth from "./UseAuth";
 import MyartCraftCard from "../Components/MyartCraftCard";
 
 const MyArtCraft = () => {
   // const {user} = useContext(AuthContext) || {};
-  const {user} = UseAuth() || {};
+  const { user } = UseAuth() || {};
 
   console.log(user);
   const [myItem, setMyItem] = useState([]);
   const [artCraft, setArtCraft] = useState(false);
-  useEffect(()=> {
+  useEffect(() => {
     fetch(`https://art-craft-store-nine.vercel.app/myProduct/${user?.email}`)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-      setMyItem(data)
-    })
-  }, [user, artCraft])
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMyItem(data);
+      });
+  }, [user, artCraft]);
   console.log(myItem);
   return (
     <div className="container mx-auto grid md:grid lg:grid-cols-3 gap-16">
-    {
-    myItem.map(items => <MyartCraftCard key={items._id} items={items} artCraft={artCraft} setArtCraft={setArtCraft}></MyartCraftCard>)
-    }
-    
+      {myItem.map((items) => (
+        <MyartCraftCard
+          key={items._id}
+          items={items}
+          artCraft={artCraft}
+          setArtCraft={setArtCraft}
+        ></MyartCraftCard>
+      ))}
     </div>
   );
 };
